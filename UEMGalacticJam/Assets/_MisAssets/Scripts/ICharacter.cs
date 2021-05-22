b35b33eb2fcd64a58a4c58ff7adde19816fbf7ad
JulenBordonaba <julenbordonaba@gmail.com>
2021-05-22 17:51:50 +0200

    GravitySystem

--- UEMGalacticJam/Assets/_MisAssets/Scripts/ICharacter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using ExtraUnityEvents;
-
-public interface ICharacter
-{
-    ReactiveProperty<Vector3> Position { get; set; }
-}
